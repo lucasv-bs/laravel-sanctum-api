dc1baa981da18a163735c1a0405e8065d1662db5
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-07-17 19:17:11 -0300

    Add the Sanctum authenticator package

--- routes/api.php
+++ routes/api.php
@@ -15,12 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('products', ProductController::class);
+// Public routes
+// Route::resource('products', ProductController::class);
+Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
 
-// Route::get('/products', [ProductController::class, 'index']);
-// Route::get('/products/{id}', [ProductController::class, 'show']);
-// Route::post('/products', [ProductController::class, 'store']);
+
+
+
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    // return $request->user();
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+});
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
